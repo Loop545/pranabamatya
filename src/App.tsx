6c872bfc55715bb848bb325d8f5f6726c695220a
pranabamatya <pranabamatya@arn.com.au>
2023-10-16 10:37:25 +1100

    prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import Lottie from "lottie-react";
+import Lottie from 'lottie-react';
 
 // Assets
 import workingOnIt from './assets/lottie/workingOnIt.json';
 
-import './App.css'
+import './App.css';
 
 function App() {
- 
   return (
     <>
       <div className="animation">
-        <Lottie 
-          animationData={workingOnIt} 
-          loop={false}
-        />
+        <Lottie animationData={workingOnIt} loop={false} />
       </div>
-      <p className="read-the-docs">
-        Something great coming....
-      </p>
+      <p className="read-the-docs">Something great coming....</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
